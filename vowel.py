67ea6247046c4c7ff2446056c4b6c12a61c6dc5f
gowshikan <raworldit@gmail.com>
2017-07-20 09:29:32 -0700

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,10 @@
+while true:
+print("enter '0' for exit.")
+ch=input("enter any character:")
+if ch == '0':
+break
+else:
+if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
+print(ch,"is a  vowel.\n")
+else:
+print(ch,"is not a vowel.\n")
